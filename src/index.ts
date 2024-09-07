eff70b633d06f2645eaaa4fb4163163f3bef7015
Sharun <sharun@sharunkumar.com>
2024-09-07 17:12:59 -0500

    use route hierarchy

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,13 @@
 import { swaggerUI } from "@hono/swagger-ui";
-import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import { logger } from "hono/logger";
+import { hello } from "./routes/hello";
 
 const app = new OpenAPIHono();
 
 app.use(logger());
 
-app.openapi(
-  createRoute({
-    method: "get",
-    path: "/hello/:name",
-    request: {
-      params: z.object({
-        name: z.string(),
-      }),
-    },
-    responses: {
-      200: {
-        description: "Respond a message",
-        content: {
-          "application/json": {
-            schema: z.object({
-              message: z.string(),
-            }),
-          },
-        },
-      },
-    },
-  }),
-  (ctx) => {
-    return ctx.json({
-      message: "hello " + ctx.req.param("name"),
-    });
-  }
-);
+app.route("/hello", hello);
 
 app.get("/ui", swaggerUI({ url: "swagger.json" }));
 
