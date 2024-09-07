6e6625715cda7bf6c445dd6119e9586975790cb3
Sharun <sharun@sharunkumar.com>
2024-09-07 18:54:31 -0500

    install zod generator and generate schemas

---
+++ prisma/generated/schemas/objects/UserWhereInput.schema.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+import { IntFilterObjectSchema } from './IntFilter.schema';
+import { StringFilterObjectSchema } from './StringFilter.schema';
+import { StringNullableFilterObjectSchema } from './StringNullableFilter.schema';
+
+import type { Prisma } from '@prisma/client';
+
+const Schema: z.ZodType<Prisma.UserWhereInput> = z
+  .object({
+    AND: z
+      .union([
+        z.lazy(() => UserWhereInputObjectSchema),
+        z.lazy(() => UserWhereInputObjectSchema).array(),
+      ])
+      .optional(),
+    OR: z
+      .lazy(() => UserWhereInputObjectSchema)
+      .array()
+      .optional(),
+    NOT: z
+      .union([
+        z.lazy(() => UserWhereInputObjectSchema),
+        z.lazy(() => UserWhereInputObjectSchema).array(),
+      ])
+      .optional(),
+    id: z.union([z.lazy(() => IntFilterObjectSchema), z.number()]).optional(),
+    email: z
+      .union([z.lazy(() => StringFilterObjectSchema), z.string()])
+      .optional(),
+    name: z
+      .union([z.lazy(() => StringNullableFilterObjectSchema), z.string()])
+      .optional()
+      .nullable(),
+  })
+  .strict();
+
+export const UserWhereInputObjectSchema = Schema;
