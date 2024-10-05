ab4b190cd12f3e0ed903c729dd0168f6f3a7bfa7
Sharun <sharun@sharunkumar.com>
2024-10-04 21:35:36 -0400

    update schema

---
+++ src/scripts/generate-schema.ts
@@ -0,0 +1,6 @@
+import { zodToJsonSchema } from "zod-to-json-schema";
+import { PlanSchema } from "../schema/schema";
+
+const planSchema = zodToJsonSchema(PlanSchema, "PlanSchema");
+
+Bun.write("src/schema/plan.schema.json", JSON.stringify(planSchema, null, 2));
