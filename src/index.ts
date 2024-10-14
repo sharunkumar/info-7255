1f3a5715bc432aa69aafaf1d5a46988217ae8eff
Sharun <sharunksplus@gmail.com>
2024-10-14 17:15:47 -0400

    deprecate: prisma and user route

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,14 @@
 import { swaggerUI } from "@hono/swagger-ui";
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { PrismaClient } from "@prisma/client";
 import { etag } from "hono/etag";
 import { logger } from "hono/logger";
-import { bootstrapDatabase } from "./functions/bootstrap-database";
 import { getRedisClient } from "./functions/get-redis-client";
-import { auth } from "./middleware/auth";
 import { plan } from "./routes/plan/plan";
-import { user } from "./routes/user";
 
 const app = new OpenAPIHono();
-const prismaClient = new PrismaClient();
 const redisClient = await getRedisClient();
 
 app.use(logger());
-
-app.use(auth(prismaClient));
-
-app.route("/user", user);
 
 app.use(etag({ weak: true }));
 
@@ -29,7 +20,5 @@ app.doc("swagger.json", {
   info: { title: "INFO7255", version: "v1" },
   openapi: "3.1.0",
 });
-
-await bootstrapDatabase(prismaClient);
 
 export default app;
