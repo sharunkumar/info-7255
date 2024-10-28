e0180991b283411f3fee4c3dddb0bf249accfe97
Sharun <sharun@sharunkumar.com>
2024-10-28 15:48:23 -0400

    fix: verify id token instead

--- src/middleware/google-token-checker.ts
+++ src/middleware/google-token-checker.ts
@@ -5,8 +5,13 @@ import { OAuth2Client } from 'google-auth-library';
 export const googleTokenVerifier = bearerAuth({
 	verifyToken: async (token, _) => {
 		try {
-			const client = new OAuth2Client();
-			const tokenInfo = await client.getTokenInfo(token);
+			const client = new OAuth2Client({
+				clientId: process.env.GOOGLE_CLIENT_ID,
+			});
+			const tokenInfo = await client.verifyIdToken({
+				idToken: token,
+				audience: client._clientId,
+			});
 			console.log({ tokenInfo });
 			return tokenInfo != null;
 		} catch (error) {
