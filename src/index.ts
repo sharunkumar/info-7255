fc987fe262ed83995ef72168e77b40c7496f3a90
Sharun <sharun@sharunkumar.com>
2024-10-29 18:45:15 -0500

    move v1 to its own file

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import { swaggerUI } from '@hono/swagger-ui';
 import { OpenAPIHono } from '@hono/zod-openapi';
-import { etag } from 'hono/etag';
 import { logger } from 'hono/logger';
-import { getRedisClient } from './functions/get-redis-client';
 import { google_token_checker } from './middleware/google-token-checker';
-import { plan } from './routes/plan/plan';
+import { v1 } from './routes/v1';
 
 const app = new OpenAPIHono();
-const redisClient = await getRedisClient();
 
 app.use(logger());
 
 app.use(google_token_checker);
 
-app.use(etag({ weak: true }));
-
-app.route('/v1/plan', plan(redisClient));
+app.route('/v1', v1);
 
 app.get('/ui', swaggerUI({ url: 'swagger.json' }));
 
