b16cd274f22b6f5864aca159c0ee6477df4a18d1
Sharun <sharun@sharunkumar.com>
2024-12-06 17:16:15 -0600

    add more elastic tests

--- src/tests/elastic.test.ts
+++ src/tests/elastic.test.ts
@@ -14,4 +14,16 @@ describe('Elasticsearch', () => {
     const response = await client.index({ index, body });
     expect(response.body.result).toBe('created');
   });
+  it('should ensure that index exists', async () => {
+    const client = await getElasticsearchClient();
+    const response = await client.indices.exists({ index });
+    expect(response.body).toBeTruthy();
+  });
+  it('should create the index if it does not exist', async () => {
+    const client1 = await getElasticsearchClient();
+    await client1.indices.delete({ index });
+    const client2 = await getElasticsearchClient();
+    const response = await client2.indices.exists({ index });
+    expect(response.body).toBeTruthy();
+  });
 });
