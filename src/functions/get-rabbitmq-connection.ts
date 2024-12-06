145bc3da1b8436ef60a07e185f1ad21d483c49fc
Sharun <715417+sharunkumar@users.noreply.github.com>
2024-12-06 11:12:51 -0800

    Implement RabbitMQ Connection (#8)

---
+++ src/functions/get-rabbitmq-connection.ts
@@ -0,0 +1,56 @@
+import amqplib from 'amqplib';
+
+type RabbitMQConnection = {
+	connection: amqplib.Connection;
+	channel: amqplib.Channel;
+	queue: string;
+	exchange: string;
+	routingKey: string;
+};
+
+export async function getRabbitMQConnection(): Promise<RabbitMQConnection> {
+	const queue = 'myQueue';
+	const exchange = 'myExchange';
+	const routingKey = 'my.routingKey';
+
+	let connection: amqplib.Connection | null = null;
+
+	while (!connection) {
+		try {
+			connection = await amqplib.connect('amqp://localhost');
+		} catch (e) {
+			console.log('Failed to connect to RabbitMQ, retrying...');
+			await Bun.sleep(500);
+		}
+	}
+
+	console.log('Connected to RabbitMQ. http://localhost:15672');
+
+	const channel = await connection.createChannel();
+
+	// Set up exchange
+	await channel.assertExchange(exchange, 'direct', { durable: true });
+
+	// Set up queue
+	await channel.assertQueue(queue, { durable: true });
+
+	// Bind queue to exchange with routing key
+	await channel.bindQueue(queue, exchange, routingKey);
+
+	// Set up consumer to forward messages to Elasticsearch
+	channel.consume(queue, async (message) => {
+		if (message) {
+			try {
+				const body = JSON.parse(message.content.toString()); // TODO: Parse using plan schema and update parents/children
+				console.log('Received message:', body);
+
+				channel.ack(message);
+			} catch (error) {
+				console.error('Error processing message:', error);
+				channel.nack(message);
+			}
+		}
+	});
+
+	return { connection, channel, queue, exchange, routingKey };
+}
--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -2,7 +2,11 @@ import { Hono } from 'hono';
 import { plan } from './plan/plan';
 import { getRedisClient } from '../functions/get-redis-client';
 import { etag } from 'hono/etag';
+import { getRabbitMQConnection } from '../functions/get-rabbitmq-connection';
+import { getElasticsearchClient } from '../functions/get-elasticsearch-client';
 
 const redisClient = await getRedisClient();
+const elasticClient = await getElasticsearchClient();
+const rabbitMQConnection = getRabbitMQConnection();
 
 export const v1 = new Hono().use(etag({ weak: true })).route('/plan', plan(redisClient));
